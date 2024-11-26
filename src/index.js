eb7afb2699f00a3455031631eb1ed6afb859e426
Ritesh mishra <riteshmishra9565@gmail.com>
2024-11-26 09:44:55 +0530

    index js se sabhi confilicts ko hata diya

--- src/index.js
+++ src/index.js
@@ -4,49 +4,34 @@ import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-<<<<<<< HEAD
-=======
 import { BrowserRouter } from "react-router-dom";
->>>>>>> react-router
+import ReactRouter from "./ReactRouter";
 
 // import App from './App';
 // import ImgGallary from './Components/ImgGallary';
 // import TestimoniaApp from './TestimoniaApp';
 // import CoursesApp from './CoursesApp';
-// import LessonThree from './LessonThree'
-<<<<<<< HEAD
+// import LessonThree from './LessonThree';
 // import HomeWorkOne from './Components/HomeWorkOne';
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <div
-    className="overflow-x-hidden h-[100vh] w-[100vw] bg-cyan-900"
-  >
+  <div className="overflow-x-hidden h-[100vh] w-[100vw] bg-gradient-to-r from-violet-300 to-fuchsia-200">
     {/* component loader */}
-    <div className="w-[90%] max-w-[1300px] mx-auto ">
-=======
-// import HomeWorkOne from './Components/?HomeWorkOne';
-import ReactRouter from "./ReactRouter";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <div className="overflow-x-hidden h-[100vh] w-[100vw] bg-gradient-to-r from-violet-300 to-fuchsia-200	">
-    {/* component loader */}
-    <div className="w-[90%] max-w-[1300px] m-auto ">
->>>>>>> react-router
+    <div className="w-[90%] max-w-[1300px] m-auto">
       <ToastContainer />
-      {/* <App></App> */}
-      {/* <ImgGallary></ImgGallary> */}
-      {/* <CoursesApp/> */}
-      {/* {<TestimoniaApp/>} */}
-      {/*<LessonThree/>*/}
-      {/* <HomeWorkOne/> */}
-<<<<<<< HEAD
-=======
       {
         <BrowserRouter>
           <ReactRouter />
         </BrowserRouter>
       }
->>>>>>> react-router
+      {/* Uncomment components as needed */}
+      {/* <App /> */}
+      {/* <ImgGallary /> */}
+      {/* <CoursesApp /> */}
+      {/* {<TestimoniaApp />} */}
+      {/* <LessonThree /> */}
+      {/* <HomeWorkOne /> */}
     </div>
   </div>
 );
